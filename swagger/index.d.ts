82235a885c33bf7082ffb3ce7d364ae124adb565
Novachocolat <lysandre.pb@gmail.com>
2026-02-13 22:22:36 +0100

    fix(swagger): update swagger directory

--- swagger/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Charge et fusionne toutes les documentations Swagger
- * @returns {object} Documentation Swagger complète
- */
-export declare function generateSwaggerSpec(): object;
-//# sourceMappingURL=index.d.ts.map
